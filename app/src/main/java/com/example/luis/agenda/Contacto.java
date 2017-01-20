5c4c2de3b8fb524a68ab10f3c2ca3a8b0d958956
luisjosetorres <lujose1112@gmail.com>
2017-01-19 23:45:48 -0600

    Creacion de pantallas para visualizar y agregar clientes y contactos

--- app/src/main/java/com/example/luis/agenda/Contacto.java
+++ app/src/main/java/com/example/luis/agenda/Contacto.java
@@ -1,13 +1,31 @@
 package com.example.luis.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Contacto extends AppCompatActivity {
 
+    Button agregar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacto);
+        agregar=(Button)findViewById(R.id.agregarContactoBtn);
+
+        agregarContacto();
+    }
+
+    public void agregarContacto(){
+        agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Contacto.this,AgregarContacto.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
